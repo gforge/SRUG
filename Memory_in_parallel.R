8c49610c89592a4a095b14c4e13e748e6f351377
Max Gordon <max@gforge.se>
2015-02-11 23:27:39 +0100

    The parallel memory under the hood

--- Memory_in_parallel.R
+++ Memory_in_parallel.R
@@ -1,9 +1,8 @@
-library("parallel")
-library("foreach")
-library("doParallel")
-
-library(pryr)
 rm(list = ls())
+library(parallel)
+library(foreach)
+library(pryr)
+
 no_cores <- detectCores()
 a <- rnorm(10^7)
 address(a)
@@ -29,3 +28,41 @@ parSapply(cl, X = 1:10, function(x) address(a)) == address(a)
 # [1] TRUE TRUE TRUE TRUE TRUE TRUE TRUE TRUE TRUE TRUE
 stopCluster(cl)
 
+# Assigning causes no change to the original
+# variable even though the address space is 
+# initially the same. 
+b <- 0
+cl<-makePSOCKcluster(2)
+clusterExport(cl, "b")
+
+parSapply(cl, X = 1:10, function(x) {b <- b + 1; b})
+# [1] 1 1 1 1 1 1 1 1 1 1
+parSapply(cl, X = 1:10, function(x) {b <<- b + 1; b})
+# [1] 1 2 3 4 5 1 2 3 4 5
+b
+stopCluster(cl)
+
+cl<-makeForkCluster(2)
+parSapply(cl, X = 1:10, function(x) {b <- b + 1; b})
+#  [1] 1 1 1 1 1 1 1 1 1 1
+parSapply(cl, X = 1:10, function(x) {b <<- b + 1; b})
+# [1] 1 2 3 4 5 1 2 3 4 5
+b
+stopCluster(cl)
+
+# Any assignment in R causes a change in address space
+cl<-makeForkCluster(4)
+out <- parLapply(cl, X = 1:4, function(x) {
+  ret <- list(start = address(b))
+  b <<- b + 1
+  ret$b <- b
+  ret$end_address <- address(b)
+  as.data.frame(ret)})
+do.call(rbind, out)
+#       start b end_address
+# 1 0x5ebe448 3   0x5d2e708
+# 2 0x5ebe448 3   0x5d21478
+# 3 0x5ebe448 3   0x5d16ab8
+# 4 0x5ebe448 3   0x5d12c28
+address(b)
+# [1] "0x5ebe448"
